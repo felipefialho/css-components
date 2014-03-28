84f69e2e50a9c7772140c619f15802b47384078d
Luiz Felipe Tartarotti Fialho <luizfelipe.tf.13@gmail.com>
2014-03-27 22:11:49 -0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,231 @@
+module.exports = function( grunt ) {
+
+// Load all tasks
+require('load-grunt-tasks')(grunt);
+
+// Paths
+var PathConfig = {
+  dev: 'dev/',
+  dist: '',
+  build: 'build/'
+};
+
+// Grunt config
+grunt.initConfig({
+
+  // Config path
+  config: PathConfig,
+
+  // Clean files
+  clean: { 
+    build: {
+      src: ['<%= config.build %>']
+    },
+    cleanzip: {
+      src: [
+        '<%= config.build %>*',
+        '!<%= config.build %>*.zip'
+      ]
+    }
+  },
+
+  // Copy files
+  copy: { 
+    build: {
+      files: [
+        {
+          expand: true,
+          flatten: true,
+          src: [
+            '<%= config.dev %>**/mixins.less',
+            '<%= config.dev %>**/variables.less',
+            '<%= config.dev %>**/collapse.less'
+          ],
+          dest: '<%= config.build %>/collapse/less/',
+          filter: 'isFile'
+        },
+        {
+          expand: true,
+          flatten: true,
+          src: [
+            '<%= config.dev %>**/mixins.less',
+            '<%= config.dev %>**/variables.less',
+            '<%= config.dev %>**/dropdown.less'
+          ],
+          dest: '<%= config.build %>/dropdown/less/',
+          filter: 'isFile'
+        },
+        {
+          expand: true,
+          flatten: true,
+          src: [
+            '<%= config.dev %>**/mixins.less',
+            '<%= config.dev %>**/variables.less',
+            '<%= config.dev %>**/modal.less'
+          ],
+          dest: '<%= config.build %>/modal/less/',
+          filter: 'isFile'
+        },
+        {
+          expand: true,
+          flatten: true,
+          src: [
+            '<%= config.dev %>**/mixins.less',
+            '<%= config.dev %>**/variables.less',
+            '<%= config.dev %>**/tab.less'
+          ],
+          dest: '<%= config.build %>/tab/less/',
+          filter: 'isFile'
+        }
+      ]
+    },
+  },
+
+  // Less
+  less: {
+    dist: {
+      options: {
+        compress: true
+      },
+      files: {
+        '<%= config.dist %>assets/css/style.css': '<%= config.dev %>assets/css/less/style.less'
+      }
+    },
+    build: {
+      files: {
+        '<%= config.build %>collapse/collapse.css': '<%= config.dev %>**/_collapse-style.less',
+        '<%= config.build %>dropdown/dropdown.css': '<%= config.dev %>**/_dropdown-style.less',
+        '<%= config.build %>modal/modal.css': '<%= config.dev %>**/_modal-style.less',
+        '<%= config.build %>tab/tab.css': '<%= config.dev %>**/_tab-style.less',
+      }
+    },
+    dev: {
+      files: {
+        '<%= config.dev %>assets/css/style.css': '<%= config.dev %>assets/css/less/style.less'
+      }
+    }
+  },
+
+  // HTMLmin
+  htmlmin: {
+    dist: {
+      options: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      files: [{
+        expand: true,
+        cwd: '<%= config.dev %>',
+        src: ['*.html','**/*.html'],
+        dest: '<%= config.dist %>',
+      }],
+    }
+  },
+
+  // Watch
+  watch : {
+    options: {
+        debounceDelay: 500,
+        livereload: true
+    },
+    less: {
+      files : [
+        '<%= config.dev %>**/*.less'
+      ],
+      tasks : ['less:dev']
+    }
+  },
+
+  // Sync
+  browser_sync: {
+    files: {
+      src : [
+        '<%= config.dev %>**/*.css',
+        '<%= config.dev %>**/*.jpg',
+        '<%= config.dev %>**/*.png',
+        '<%= config.dev %>**/*.js',
+        '<%= config.dev %>*.html'
+      ]
+    },
+    options: {
+      watchTask: true,
+      host : '',
+      server: {
+        baseDir: '<%= config.dev %>'
+      },
+      ghostMode: {
+        scroll: true,
+        links: true,
+        forms: true
+      }
+    }
+  },
+
+  // Make a Zipfile
+  compress: {
+    collapse: {
+      options: {
+        archive: '<%= config.build %>collapse.zip'
+      },
+      files: [
+        {
+          expand: true,
+          cwd: '<%= config.build %>',
+          src: ['collapse/**']
+        }
+      ]
+    },
+    dropdown: {
+      options: {
+        archive: '<%= config.build %>dropdown.zip'
+      },
+      files: [
+        {
+          expand: true,
+          cwd: '<%= config.build %>',
+          src: ['dropdown/**']
+        }
+      ]
+    },
+    modal: {
+      options: {
+        archive: '<%= config.build %>modal.zip'
+      },
+      files: [
+        {
+          expand: true,
+          cwd: '<%= config.build %>',
+          src: ['modal/**']
+        }
+      ]
+    },
+    tab: {
+      options: {
+        archive: '<%= config.build %>tab.zip'
+      },
+      files: [
+        {
+          expand: true,
+          cwd: '<%= config.build %>',
+          src: ['tab/**']
+        }
+      ]
+    }
+  }
+
+});
+
+// JsLint
+grunt.registerTask( 'test', ['jshint'] );
+
+// Dist
+grunt.registerTask( 'dist', ['less:dist', 'htmlmin:dist'] );
+
+// Build
+grunt.registerTask( 'build', ['clean:build', 'copy:build', 'less:build', 'compress', 'clean:cleanzip'] );
+
+// Watch
+grunt.registerTask( 'w', ['browser_sync', 'watch' ] );
+
+
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+Pure CSS Components
+=========
+
+A repository of components development in CSS only, no Javascript.
+
+[View the project](http://www.felipefialho.com/css-components)
+
+## License
+ 
+[MIT License](http://felipefialho.mit-license.org/) © Luiz Felipe Tartarotti Fialho
